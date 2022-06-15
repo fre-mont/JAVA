23e87b66b49c8461fe6d40b7ff9e8c2bb8db5517
Lucysgy <87704651+Lucysgy@users.noreply.github.com>
2022-06-16 08:49:51 +0900

    a

---
+++ Homework/HW5/src/derived/v.java
@@ -0,0 +1,10 @@
+package derived;
+import java.util.Vector;
+
+public class v {
+    public static void main(String[] args) {
+        Vector<Integer> v = new Vector<Integer>();
+        System.out.println(v.capacity());
+    }
+    
+}
